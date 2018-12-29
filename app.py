32002d01ea31284f8db3de867564cf8626e8556b
hakaneroztekin <eroztekin@itu.edu.tr>
2018-12-29 08:18:47 +0300

    add implementation steps

---
+++ app.py
@@ -0,0 +1,17 @@
+# Scrap and Visualize Data
+# Data Source: https://yokatlas.yok.gov.tr/lisans-anasayfa.php
+#
+# For all the universities in Turkey that has Computer Engineering undergraduate program,
+# visualize the following for year 2017:
+# 1- Use boxplot to visualize quote ("kontenjan") wrt. region of universities
+# 2- Use scatterplot to visualize the relation btw. average net math questions in YGS and lowest student rank.
+#    Find r square for this relation.
+#
+# Implementation Steps:
+# 1- Scrap the data from the source.
+# 2- Improve the data scraping
+# 3- Use boxplot
+# 4- Use scatterplot
+# 5- Wrap-up & Upload the project
+#
+
