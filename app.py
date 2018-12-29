2700162365f9af6c480d0b7db607318a11767fb8
hakaneroztekin <eroztekin@itu.edu.tr>
2018-12-29 21:00:04 +0300

    update implementation steps and complete the project

--- app.py
+++ app.py
@@ -1,26 +1,5 @@
-# Scrap and Visualize Data
-# Data Source: https://yokatlas.yok.gov.tr/lisans-bolum.php?b=10024
-#
-# For all the universities in Turkey that has Computer Engineering undergraduate program,
-# visualize the following for year 2017:
-# 1- Use boxplot to visualize quote ("kontenjan") wrt. region of universities
-# 2- Use scatterplot to visualize the relation btw. average net math questions in YGS and lowest student rank.
-#    Find r square for this relation.
-#
-# Implementation Steps:
-# + 1- Scrap the data from the source.
-# + 2- Improve the data scraping
-# > +Get clear URL's for each university
-# > +Get Quota for Specific University
-# > +Get University's City
-# > +Get Quota & City for All Universities'
-#
-# + 3- Use boxplot
-#
-# 4- Use scatterplot
-#
-# 5- Wrap-up & Upload the project
-#
+# Python Web Scraping Project
+# 29.12.18
 
 import re
 from contextlib import closing
